70eee4c3c3d343bb0ed3da66494abc4fec3cd209
Alex-Shellshock <bryan.r@weareshellshock.com>
2021-08-01 17:39:17 -0500

    Footer with top section

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -16,91 +16,109 @@ import Container from "@material-ui/core/Container"
 const Footer = ({ className }) => {
   return (
     <S.Wrapper className={className}>
-      <S.LogoWrapper xs={12}>
-        <img
-          src="https://admin-a-christianglass.weareshellshock.com/wp-content/uploads/2021/08/logo-White.png"
-          alt="ACG logo"
-        />
-      </S.LogoWrapper>
-      <S.SeparatorWrapper>
-        <Separator />
-      </S.SeparatorWrapper>
-      <S.LinksWrapper>
-        <S.LeftContainer>
-          <S.ProductsWrapper>
-            <S.LinksContainer>
-              <h3>Products</h3>
-              <a href="#">Windows</a>
-              <a href="#">Doors</a>
-              <a href="#">Showers</a>
-              <a href="#">Railway</a>
-            </S.LinksContainer>{" "}
-            <S.LinksContainer>
-              <h3>Industries</h3>
-              <a href="#">Residential</a>
-              <a href="#">Commercial</a>
-            </S.LinksContainer>{" "}
-            <S.LinksContainer>
-              <h3>About</h3>
-              <a href="#">News</a>
-              <a href="#">Projects</a>
-            </S.LinksContainer>
-          </S.ProductsWrapper>{" "}
+      <S.GrayContainer />
+      <S.ExperienceWrapper>
+        <S.ExperienceTextWrapper>
+          <S.ExperienceContent>
+            <h3>A-Christian Glass</h3>
+            <h4>Experience Matters</h4>
+            <p>
+              Our experienced installation team takes precise measurements so
+              your new windows and doors are a perfect fit. We are meticulous to
+              ensure you are 100% satisfied with your new impact resistant
+              windows and doors.
+            </p>
+            <S.Button>Schedule an Appointment</S.Button>
+          </S.ExperienceContent>
+        </S.ExperienceTextWrapper>
+      </S.ExperienceWrapper>
+      <S.FooterContainer>
+        <S.LogoWrapper xs={12}>
           <img
-            src="https://admin-a-christianglass.weareshellshock.com/wp-content/uploads/2021/08/BBB-seal-footer.png"
-            alt="BBC Logo"
+            src="https://admin-a-christianglass.weareshellshock.com/wp-content/uploads/2021/08/logo-White.png"
+            alt="ACG logo"
           />
-        </S.LeftContainer>
+        </S.LogoWrapper>
+        <S.SeparatorWrapper>
+          <Separator />
+        </S.SeparatorWrapper>
+        <S.LinksWrapper>
+          <S.LeftContainer>
+            <S.ProductsWrapper>
+              <S.LinksContainer>
+                <h3>Products</h3>
+                <a href="#">Windows</a>
+                <a href="#">Doors</a>
+                <a href="#">Showers</a>
+                <a href="#">Railway</a>
+              </S.LinksContainer>{" "}
+              <S.LinksContainer>
+                <h3>Industries</h3>
+                <a href="#">Residential</a>
+                <a href="#">Commercial</a>
+              </S.LinksContainer>{" "}
+              <S.LinksContainer>
+                <h3>About</h3>
+                <a href="#">News</a>
+                <a href="#">Projects</a>
+              </S.LinksContainer>
+            </S.ProductsWrapper>{" "}
+            <img
+              src="https://admin-a-christianglass.weareshellshock.com/wp-content/uploads/2021/08/BBB-seal-footer.png"
+              alt="BBC Logo"
+            />
+          </S.LeftContainer>
 
-        <Container>
-          <S.InfoWrapper>
-            <S.ContactWrapper>
-              <span>Call Us: </span>
-              <a href="#">(561) 278-3385</a>
-            </S.ContactWrapper>
-            <S.LocationWrapper>
-              <LocationIcon />
-              <p>925 S. Congress AvenueDelray Beach, FL 33445</p>
-            </S.LocationWrapper>
-            <S.MailWrapper>
-              <MailIcon />
-              <a href="">info@a-christianglass.com</a>
-            </S.MailWrapper>
-            <S.SocialIconsWrapper>
-              <h3>Follow Us</h3>
-              <S.IconRow>
-                <S.Logo>
-                  <a href="#yt" target="_blank">
-                    <YouTubeIcon />
-                  </a>{" "}
-                  <a href="#fb" target="_blank">
-                    <FacebookIcon />
-                  </a>
-                  <a href="#ins" target="_blank">
-                    <InstagramIcon />
-                  </a>
-                  <a href="#yelp" target="_blank">
-                    <YelpIcon />
-                  </a>
-                  <a href="#pint" target="_blank">
-                    <PinterestIcon />
-                  </a>
-                  <a href="#lind" target="_blank">
-                    <LinkedInIcon />
-                  </a>
-                </S.Logo>
-              </S.IconRow>
-            </S.SocialIconsWrapper>
-            <S.FaqWrapper>
-              <a href="#">FAQ</a>
-              <Rectangle />
-              <a href="#">Privacy Policy</a>
-              <Rectangle />
-              <a href="#">EQUE</a>
-            </S.FaqWrapper>
-          </S.InfoWrapper>
-        </Container>
-      </S.LinksWrapper>
+          <Container>
+            <S.InfoWrapper>
+              <S.ContactWrapper>
+                <span>Call Us: </span>
+                <a href="#">(561) 278-3385</a>
+              </S.ContactWrapper>
+              <S.LocationWrapper>
+                <LocationIcon />
+                <p>925 S. Congress AvenueDelray Beach, FL 33445</p>
+              </S.LocationWrapper>
+              <S.MailWrapper>
+                <MailIcon />
+                <a href="">info@a-christianglass.com</a>
+              </S.MailWrapper>
+              <S.SocialIconsWrapper>
+                <h3>Follow Us</h3>
+                <S.IconRow>
+                  <S.Logo>
+                    <a href="#yt" target="_blank">
+                      <YouTubeIcon />
+                    </a>{" "}
+                    <a href="#fb" target="_blank">
+                      <FacebookIcon />
+                    </a>
+                    <a href="#ins" target="_blank">
+                      <InstagramIcon />
+                    </a>
+                    <a href="#yelp" target="_blank">
+                      <YelpIcon />
+                    </a>
+                    <a href="#pint" target="_blank">
+                      <PinterestIcon />
+                    </a>
+                    <a href="#lind" target="_blank">
+                      <LinkedInIcon />
+                    </a>
+                  </S.Logo>
+                </S.IconRow>
+              </S.SocialIconsWrapper>
+              <S.FaqWrapper>
+                <a href="#">FAQ</a>
+                <Rectangle />
+                <a href="#">Privacy Policy</a>
+                <Rectangle />
+                <a href="#">EQUE</a>
+              </S.FaqWrapper>
+            </S.InfoWrapper>
+          </Container>
+        </S.LinksWrapper>
+      </S.FooterContainer>
     </S.Wrapper>
   )
 }
--- src/components/footer/footer.styles.jsx
+++ src/components/footer/footer.styles.jsx
@@ -1,10 +1,75 @@
 import styled from "styled-components"
 import { Grid } from "@material-ui/core"
 import Container from "@material-ui/core/Container"
+import CustomButton from "../custom-button/custom-button.component"
 
+export const GrayContainer = styled.div`
+  min-height: 500px;
+  background-color: #f0f5f7;
+`
+export const ExperienceWrapper = styled.div`
+  min-height: 500px;
+  background: linear-gradient(
+    to bottom,
+    #f0f5f7 0%,
+    #f0f5f7 75%,
+    radial-gradient(56.04% 163.52% at 66.32% 139.24%, #4fbff3 0%, #1593cd 100%)
+      50%,
+    radial-gradient(56.04% 163.52% at 66.32% 139.24%, #4fbff3 0%, #1593cd 100%)
+      50%,
+    100%
+  );
+`
+export const Button = styled(CustomButton)`
+  margin-top: 2em;
+  padding: 0.5em 1em;
+  background-color: ${({ theme }) => theme.palette.tertiary.main};
+  text-transform: capitalize;
+`
+
+export const ExperienceTextWrapper = styled.div`
+  min-height: 465px;
+  margin: -25em 6em 0 6em;
+  display: flex;
+  justify-content: flex-end;
+  background-image: url("https://admin-a-christianglass.weareshellshock.com/wp-content/uploads/2021/08/install-.win@1.5x.png");
+  background-size: cover;
+`
+
+export const ExperienceContent = styled.div`
+  padding: 2em;
+  margin: auto 150px;
+  max-width: 445px;
+  max-height: 465px;
+  color: black;
+  h3 {
+    color: rgba(29, 56, 69, 0.75);
+    opacity: 0.5;
+    text-transform: uppercase;
+    font-size: 14px;
+    margin-bottom: 1em;
+  }
+  h4 {
+    color: ${({ theme }) => theme.palette.text.secondary};
+    font-size: 2rem;
+    font-weight: bold;
+    margin-bottom: 0.5em;
+  }
+  p {
+    margin: 0;
+    color: rgba(29, 56, 69, 0.75);
+    font-size: 1rem;
+  }
+`
+
+export const FooterContainer = styled(Container)`
+  padding-left: 0;
+  padding-right: 0;
+  margin-top: -8em;
+`
 export const Wrapper = styled.footer`
   color: white;
-  padding: 4em 0 0;
+  position: relative;
   background: radial-gradient(
     56.04% 163.52% at 66.32% 139.24%,
     #4fbff3 0%,
