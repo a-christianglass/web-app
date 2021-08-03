4d8da9e6d3d0dd949f64e3ce262484d573e1ae7a
Alex-Shellshock <bryan.r@weareshellshock.com>
2021-08-03 12:04:52 -0500

    remove import and added html parser

--- src/layouts/home-hero/home-hero.component.jsx
+++ src/layouts/home-hero/home-hero.component.jsx
@@ -1,6 +1,6 @@
 import React from "react"
-import { HeroWrapper } from "./home-hero.styles"
 import * as S from "./home-hero.styles"
+import parse from "html-react-parser"
 import CustomBgImage from "../../components/custom-bg-image/custom-bg-image.component"
 
 const HomeHero = ({ ...props }) => {
@@ -10,8 +10,8 @@ const HomeHero = ({ ...props }) => {
       <CustomBgImage img={image}>
         <S.ContentWrapper>
           <S.TextWrapper>
-            <h2>{subtitle}</h2>
-            <h1>{title}</h1>
+            <h2>{parse(subtitle)}</h2>
+            <h1>{parse(title)}</h1>
             <S.Button href={button && button.url}>
               {button && button.title}
             </S.Button>
