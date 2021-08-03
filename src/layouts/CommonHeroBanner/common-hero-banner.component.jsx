f03b5a90feeeef21ae9875fd73709e1f86124124
Alex-Shellshock <bryan.r@weareshellshock.com>
2021-08-03 15:43:48 -0500

    remove import

--- src/layouts/CommonHeroBanner/common-hero-banner.component.jsx
+++ src/layouts/CommonHeroBanner/common-hero-banner.component.jsx
@@ -1,7 +1,6 @@
 import React from "react"
 import * as S from "./common-hero-banner.styles"
 import parse from "html-react-parser"
-import CustomBgImage from "../../components/custom-bg-image/custom-bg-image.component"
 
 const CommonHeroBanner = ({ image, title, content }) => {
   return (
