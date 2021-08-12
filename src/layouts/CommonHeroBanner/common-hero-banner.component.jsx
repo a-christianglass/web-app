49c47ffe3ea302e232433a290a41005ee65e62b1
Alex-Shellshock <bryan.r@weareshellshock.com>
2021-08-12 15:33:25 -0500

    common hero minor fixes

--- src/layouts/CommonHeroBanner/common-hero-banner.component.jsx
+++ src/layouts/CommonHeroBanner/common-hero-banner.component.jsx
@@ -1,16 +1,21 @@
 import React from "react"
 import * as S from "./common-hero-banner.styles"
 import parse from "html-react-parser"
+import { Grid } from "@material-ui/core"
 
 const CommonHeroBanner = ({ image, title, content }) => {
   return (
     <S.Wrapper>
-      <S.TopWrapper>
-        <S.TextWrapper>
-          {title && <h1>{parse(title)}</h1>}
-          {content && <h2>{parse(content)}</h2>}
-        </S.TextWrapper>
-      </S.TopWrapper>
+      <Grid container>
+        <S.TopWrapper>
+          <Grid item xs={12}>
+            {title && <h1>{parse(title)}</h1>}
+          </Grid>
+          <Grid item xs={12}>
+            {content && <h2>{parse(content)}</h2>}
+          </Grid>
+        </S.TopWrapper>
+      </Grid>
       {image && <S.BottomWrapper img={image} />}
     </S.Wrapper>
   )
--- src/layouts/CommonHeroBanner/common-hero-banner.styles.jsx
+++ src/layouts/CommonHeroBanner/common-hero-banner.styles.jsx
@@ -5,32 +5,31 @@ import CustomImage from "../../components/custom-image/custom-image.component"
 export const Wrapper = styled(SectionWrapper)`
   padding: 0;
 `
+
 export const TopWrapper = styled.div`
   max-height: 488px;
-  padding: 2em;
+  padding: 6em 10em;
   background: radial-gradient(
     56.04% 163.52% at 66.32% 139.24%,
     #4fbff3 0%,
     #1593cd 100%
   );
-`
 
-export const TextWrapper = styled.div`
-  max-width: 950px;
-  padding: 10em 0 2em 10em;
-  ${({ theme }) => theme.breakpoints.down("sm")} {
-    padding: 2em;
-  }
   h1 {
     font-size: 0.875rem;
     margin-bottom: 1em;
     text-transform: uppercase;
   }
+
   h2 {
     font-size: 2.25rem;
     ${({ theme }) => theme.breakpoints.down("sm")} {
       font-size: 1rem;
     }
+  }
+
+  ${({ theme }) => theme.breakpoints.down("sm")} {
+    padding: 2em;
   }
 `
 
