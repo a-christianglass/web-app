d020fcea54ed79fb119bc3af7995379be1e0c018
Alex-Shellshock <bryan.r@weareshellshock.com>
2021-08-19 15:03:45 -0500

    miscContent padding

--- src/layouts/MiscContent/misc-content.component.jsx
+++ src/layouts/MiscContent/misc-content.component.jsx
@@ -1,12 +1,15 @@
 import React from "react"
 import * as S from "./misc-content.styles.jsx"
 import parse from "html-react-parser"
+import Container from "@material-ui/core/Container"
 
 const MiscContent = ({ containerMaxWidth, content }) => {
   if (!content) return null
   return (
     <S.Wrapper maxWidth={containerMaxWidth ? containerMaxWidth : false}>
-      <S.TextWrapper>{parse(content)}</S.TextWrapper>
+      <S.ContentWrapper>
+        <S.TextWrapper>{parse(content)}</S.TextWrapper>
+      </S.ContentWrapper>
     </S.Wrapper>
   )
 }
