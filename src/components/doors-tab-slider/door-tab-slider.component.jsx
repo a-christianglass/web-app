ed8fdb2b14b95db113ccac554bcaadef791c628d
Bryan Valladares <bryanshellshock@users.noreply.github.com>
2021-08-24 15:55:08 -0500

    fix pagination doorsTabs

--- src/components/doors-tab-slider/door-tab-slider.component.jsx
+++ src/components/doors-tab-slider/door-tab-slider.component.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import { Swiper, SwiperSlide } from "swiper/react"
+import * as S from "./door-tab-slider.styles"
 
 // Import Swiper styles
 import "swiper/swiper.min.css"
 import "swiper/components/pagination/pagination.min.css"
 import SwiperCore, { Pagination } from "swiper/core"
 import CustomImage from "../custom-image/custom-image.component"
+import { ArrowBack, ArrowForward } from "@material-ui/icons"
 
 // install Swiper modules
 SwiperCore.use([Pagination])
@@ -13,9 +15,13 @@ SwiperCore.use([Pagination])
 const DoorTabSlider = ({ images }) => {
   if (!images) return null
   return (
-    <Swiper
+    <S.CustomSwiper
       pagination={{
+        el: '.my-swiper-pagination',
         clickable: true,
+        renderBullet: function (index, className) {
+          return '<span class="' + className + '">' + ([index]) + '</span>';
+        },
       }}
       slidesPerView="1"
       loop={true}
@@ -26,7 +32,8 @@ const DoorTabSlider = ({ images }) => {
           {image && <CustomImage img={image} />}
         </SwiperSlide>
       ))}
-    </Swiper>
+      <div className="my-swiper-pagination"/>
+    </S.CustomSwiper>
   )
 }
 
---
+++ src/components/doors-tab-slider/door-tab-slider.styles.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+import { Swiper } from "swiper/react"
+
+export const CustomSwiper = styled(Swiper)`
+  .my-swiper-pagination {
+    padding-top: 3em;
+    text-align: center;
+  }
+`
