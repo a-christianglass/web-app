f49c2587a89d1d1f9f34a67e8d886919ad2c97dc
Alex-Shellshock <bryan.r@weareshellshock.com>
2021-08-25 10:33:14 -0500

    REFACTOR: mobile call us banner layout

--- src/layouts/CallUsBanner/call-us-banner.component.jsx
+++ src/layouts/CallUsBanner/call-us-banner.component.jsx
@@ -3,49 +3,68 @@ import * as S from "./call-us-banner.styles"
 import PhoneIcon from "../../assets/icons/phone.svg"
 import CustomBgImage from "../../components/custom-bg-image/custom-bg-image.component"
 import parse from "html-react-parser"
-import Grid from "@material-ui/core/Grid";
+import Grid from "@material-ui/core/Grid"
+import theme from "../../theme"
+import useMediaQuery from "@material-ui/core/useMediaQuery"
 
-const CallUsBanner = ({ imageCallBanner, title, number, content }) => {
+const CallUsBanner = ({
+  imageCallBanner,
+  mobileImageCallBanner,
+  title,
+  number,
+  content,
+}) => {
   // No content == more padding in TextWrapper and text align: left on Content Wrapper
   const morePadding = content === undefined
+  const isMD = useMediaQuery(theme.breakpoints.down("md"))
   return (
     <S.Wrapper>
-      <CustomBgImage img={imageCallBanner}>
+      {!isMD ? (
+        <CustomBgImage img={imageCallBanner}>
           <Grid container>
-              <Grid item xs={12} md={6}/>
-              <Grid item xs={12} md={6}>
-                  <S.TextWrapper morePadding={morePadding}>
-                      <S.ContentWrapper textAlign={morePadding}>
-                          {title && <h2>{title}</h2>}
-                          {number && (
-                              <a href="tel:+5612783385">
-                                  <S.NumberWrapper>
-                                      <PhoneIcon />
-                                      <span>{number}</span>
-                                  </S.NumberWrapper>
-                              </a>
-                          )}
-                          {content && <h3>{parse(content)}</h3>}
-                      </S.ContentWrapper>
-                  </S.TextWrapper>
-              </Grid>
+            <Grid item xs={12} md={6} />
+            <Grid item xs={12} md={6}>
+              <S.TextWrapper morePadding={morePadding}>
+                <S.ContentWrapper textAlign={morePadding}>
+                  {title && <h2>{title}</h2>}
+                  {number && (
+                    <a href="tel:+5612783385">
+                      <S.NumberWrapper>
+                        <PhoneIcon />
+                        <span>{number}</span>
+                      </S.NumberWrapper>
+                    </a>
+                  )}
+                  {content && <h3>{parse(content)}</h3>}
+                </S.ContentWrapper>
+              </S.TextWrapper>
+            </Grid>
           </Grid>
-
-        {/*<S.TextWrapper morePadding={morePadding}>*/}
-        {/*  <S.ContentWrapper textAlign={morePadding}>*/}
-        {/*    {title && <h2>{title}</h2>}*/}
-        {/*    {number && (*/}
-        {/*      <a href="tel:+5612783385">*/}
-        {/*        <S.NumberWrapper>*/}
-        {/*          <PhoneIcon />*/}
-        {/*          <span>{number}</span>*/}
-        {/*        </S.NumberWrapper>*/}
-        {/*      </a>*/}
-        {/*    )}*/}
-        {/*    {content && <h3>{parse(content)}</h3>}*/}
-        {/*  </S.ContentWrapper>*/}
-        {/*</S.TextWrapper>*/}
-      </CustomBgImage>
+        </CustomBgImage>
+      ) : (
+        <Grid container direction="column">
+          <Grid item>
+            <S.MobileBgImage img={mobileImageCallBanner}>
+              <S.MobileTextWrapper>
+                {title && <h2>{title}</h2>}
+              </S.MobileTextWrapper>
+            </S.MobileBgImage>
+          </Grid>
+          <Grid item>
+            <S.MobileContentWrapper>
+              {number && (
+                <a href="tel:+5612783385">
+                  <S.NumberWrapper>
+                    <PhoneIcon />
+                    <span>{number}</span>
+                  </S.NumberWrapper>
+                </a>
+              )}
+              {content && <h3>{parse(content)}</h3>}
+            </S.MobileContentWrapper>
+          </Grid>
+        </Grid>
+      )}
     </S.Wrapper>
   )
 }
