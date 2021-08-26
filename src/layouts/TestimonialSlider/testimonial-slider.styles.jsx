a723c9866d2f4b3a46a80e94eb31baa4f066723f
Alex-Shellshock <bryan.r@weareshellshock.com>
2021-08-26 16:01:10 -0500

    REFACTOR: mobile testimonial slider

--- src/layouts/TestimonialSlider/testimonial-slider.styles.jsx
+++ src/layouts/TestimonialSlider/testimonial-slider.styles.jsx
@@ -8,7 +8,7 @@ export const Wrapper = styled(SectionWrapper)`
   background-color: #f0f5f7;
   position: relative;
   &:before {
-    content: '';
+    content: "";
     position: absolute;
     width: 337px;
     height: 295px;
@@ -16,7 +16,11 @@ export const Wrapper = styled(SectionWrapper)`
     right: 0;
     z-index: 1;
     pointer-events: none;
-    background: linear-gradient(270deg, #FFFFFF 11.36%, rgba(255, 255, 255, 0) 59.47%);
+    background: linear-gradient(
+      270deg,
+      #ffffff 11.36%,
+      rgba(255, 255, 255, 0) 59.47%
+    );
   }
 
   .swiper-container {
@@ -38,27 +42,38 @@ export const TextWrapper = styled.div`
     color: rgba(13, 46, 77, 0.75);
     letter-spacing: 0.06em;
     line-height: 17px;
-    font-size: 14px;
+    font-size: ${({ theme }) => theme.typography.pxToRem(14)};
     opacity: 0.74;
     margin-bottom: 1em;
   }
   h2 {
-    font-size: 36px;
+    font-size: ${({ theme }) => theme.typography.pxToRem(36)};
     line-height: 47px;
     color: #0d5c80;
     margin-bottom: 0.5em;
+    ${({ theme }) => theme.breakpoints.down("sm")} {
+      font-size: ${({ theme }) => theme.typography.pxToRem(24)};
+      line-height: 29px;
+    }
   }
   h4 {
-    font-size: 16px;
+    font-size: ${({ theme }) => theme.typography.pxToRem(16)};
     line-height: 26px;
     color: rgba(29, 56, 69, 0.75);
     font-style: normal;
     font-weight: 500;
     margin-bottom: 1em;
+    ${({ theme }) => theme.breakpoints.down("sm")} {
+      font-size: ${({ theme }) => theme.typography.pxToRem(14)};
+    }
   }
 `
 export const LeftGrid = styled(Grid)`
   padding: 2em;
+  ${({ theme }) => theme.breakpoints.down("sm")} {
+    padding: 2em 0;
+  }
+
   display: flex;
   justify-content: center;
   align-items: center;
@@ -96,6 +111,6 @@ export const CardWrapper = styled.div`
   }
 `
 
-export const RightGrid  = styled(Grid)`
-  clip-path: inset( -100vw -100vw -100vw 0 );
+export const RightGrid = styled(Grid)`
+  clip-path: inset(-100vw -100vw -100vw 0);
 `
