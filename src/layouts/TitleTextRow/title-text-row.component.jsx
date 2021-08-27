c49809c06252cfb5e3867d3ce8ec646c65ebb1ba
Alex-Shellshock <bryan.r@weareshellshock.com>
2021-08-27 14:51:43 -0500

    container added

--- src/layouts/TitleTextRow/title-text-row.component.jsx
+++ src/layouts/TitleTextRow/title-text-row.component.jsx
@@ -2,20 +2,25 @@ import React from "react"
 import * as S from "./title-text-row.styles"
 import { Grid } from "@material-ui/core"
 import parse from "html-react-parser"
+import Container from "@material-ui/core/Container"
 
 const TitleTextRow = ({ title, contentTitleTextRow }) => {
   return (
     <S.Wrapper>
-      <Grid container>
-        <S.LeftGrid item xs={12} md={6}>
-          <S.LeftTextWrapper>{title && <h2>{title}</h2>}</S.LeftTextWrapper>
-        </S.LeftGrid>
-        <S.RightGrid item xs={12} md={6}>
-          <S.RightTextWrapper>
-            {contentTitleTextRow && <S.ContentText>{parse(contentTitleTextRow)}</S.ContentText>}
-          </S.RightTextWrapper>
-        </S.RightGrid>
-      </Grid>
+      <Container>
+        <Grid container>
+          <S.LeftGrid item xs={12} md={6}>
+            <S.LeftTextWrapper>{title && <h2>{title}</h2>}</S.LeftTextWrapper>
+          </S.LeftGrid>
+          <S.RightGrid item xs={12} md={6}>
+            <S.RightTextWrapper>
+              {contentTitleTextRow && (
+                <S.ContentText>{parse(contentTitleTextRow)}</S.ContentText>
+              )}
+            </S.RightTextWrapper>
+          </S.RightGrid>
+        </Grid>
+      </Container>
     </S.Wrapper>
   )
 }
