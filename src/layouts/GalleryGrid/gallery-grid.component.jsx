db927ef1f4f5fc4a3f14a85601bb96ee8779414c
Alex-Shellshock <bryan.r@weareshellshock.com>
2021-08-27 16:18:22 -0500

    shower grid minor fix

--- src/layouts/GalleryGrid/gallery-grid.component.jsx
+++ src/layouts/GalleryGrid/gallery-grid.component.jsx
@@ -1,17 +1,19 @@
 import React from "react"
 import * as S from "./gallery-grid.styles"
 import Container from "@material-ui/core/Container"
-import { Grid } from "@material-ui/core"
-import { CustomLink } from "../../components/custom-link/custom-link.styles"
+import { Grid, useTheme } from "@material-ui/core"
 import CustomImage from "../../components/custom-image/custom-image.component"
-import Arrow from "../../assets/icons/arrow.svg"
+import useMediaQuery from "@material-ui/core/useMediaQuery"
 
 const GalleryGrid = ({ title, cards }) => {
+  const theme = useTheme()
+  const isMD = useMediaQuery(theme.breakpoints.up("md"))
+
   return (
     <S.Wrapper>
       <Container>
         {title && <S.Title>{title}</S.Title>}
-        <Grid container>
+        <Grid container spacing={isMD ? 5 : 2}>
           {cards.map(({ image, title }, index) => (
             <S.BottomGrid item xs={6} md={4} key={`project-card-${index}`}>
               <S.CardContainer>
