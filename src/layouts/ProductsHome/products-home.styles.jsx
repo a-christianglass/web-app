55b60e9e8f913fae87b17186e796c0e89a8e50cb
Alex-Shellshock <bryan.r@weareshellshock.com>
2021-08-31 13:29:53 -0500

    products home update

--- src/layouts/ProductsHome/products-home.styles.jsx
+++ src/layouts/ProductsHome/products-home.styles.jsx
@@ -2,10 +2,20 @@ import styled from "styled-components"
 import SectionWrapper from "../../components/section-wrapper/section-wrapper.component"
 import CustomBgImage from "../../components/custom-bg-image/custom-bg-image.component"
 import CustomImage from "../../components/custom-image/custom-image.component"
+import Container from "@material-ui/core/Container"
 
 export const Wrapper = styled(SectionWrapper)`
-  padding: 0;
+  ${({ theme }) => theme.breakpoints.up("md")} {
+    padding: 2em 0;
+  }
 `
+
+export const CustomContainer = styled(Container)`
+  ${({ theme }) => theme.breakpoints.down("sm")} {
+    padding: 0;
+  }
+`
+
 export const TextWrapper = styled.div`
   ${({ theme }) => theme.breakpoints.up("md")} {
     max-width: 380px;
@@ -36,6 +46,10 @@ export const TextWrapper = styled.div`
 export const ProductImage = styled(CustomImage)`
   max-height: 390px !important;
   width: auto;
+  height: 100%;
+  ${({ theme }) => theme.breakpoints.down("sm")} {
+    max-height: 200px !important;
+  }
   img {
     max-height: 390px !important;
   }
