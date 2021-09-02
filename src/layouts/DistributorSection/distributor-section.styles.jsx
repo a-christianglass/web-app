03c4ebe63ba73803e3360b102d047a5e96d8ba9c
AlexShellshock <bryan.r@weareshellshock.com>
2021-09-02 00:28:29 -0500

    distributor section padding update

--- src/layouts/DistributorSection/distributor-section.styles.jsx
+++ src/layouts/DistributorSection/distributor-section.styles.jsx
@@ -4,6 +4,7 @@ import { Grid, Typography } from "@material-ui/core"
 import CustomImage from "../../components/custom-image/custom-image.component"
 
 export const Wrapper = styled(SectionWrapper)`
+  padding: 10em 0;
   ${({ bgColor }) =>
     bgColor === "white"
       ? `
@@ -14,6 +15,9 @@ export const Wrapper = styled(SectionWrapper)`
           background-color:  #f0f5f7;
 
    `};
+  ${({ theme }) => theme.breakpoints.down("sm")} {
+    padding: 2em 0;
+  }
 `
 
 export const LeftGrid = styled(Grid)`
