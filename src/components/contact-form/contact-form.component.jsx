2fe885a47e4bf059610c2437b6b9ffc629f792a2
Bryan Valladares <bryanshellshock@users.noreply.github.com>
2021-09-03 11:05:45 -0500

    form connection to wordpress

--- src/components/contact-form/contact-form.component.jsx
+++ src/components/contact-form/contact-form.component.jsx
@@ -1,12 +1,42 @@
 import React, { useState } from "react"
 import * as S from "./contact-form.styles"
-import { FormControl, InputAdornment } from "@material-ui/core"
+import { FormControl, InputAdornment, Typography } from "@material-ui/core"
 import PersonIcon from "../../assets/icons/person.svg"
 import MailIcon from "../../assets/icons/mailForm.svg"
 import PhoneIcon from "../../assets/icons/phoneForm.svg"
+import { useForm, Controller } from "react-hook-form"
+import { contactFormApi } from "../../apis/apis"
 
 const ContactForm = () => {
   const [products, setProducts] = useState("")
+  const { register, handleSubmit, errors, control } = useForm({
+    mode: "onBlur",
+    reValidateMode: "onBlur",
+  })
+
+  const [isLoading, setIsLoading] = useState(false)
+  const [successMessage, setSuccessMessage] = useState("")
+
+  const onSubmit = async data => {
+    setIsLoading(true)
+    const form = new FormData()
+    form.append("yourName", data.yourName)
+    form.append("yourEmail", data.yourEmail)
+    form.append("yourPhone", data.yourPhone)
+    form.append("product", data.product)
+    form.append("yourMessage", data.yourMessage)
+
+    contactFormApi
+      .post("/971/feedback", form, {
+        headers: { "Content-Type": "multipart/form-data" },
+      })
+      .then(response => {
+        setIsLoading(false)
+        if (response.data) {
+          setSuccessMessage(response.data.message)
+        }
+      })
+  }
 
   const handleChange = event => {
     setProducts(event.target.value)
@@ -14,79 +44,107 @@ const ContactForm = () => {
 
   return (
     <S.FormWrapper>
-      {/*<FormControl>*/}
-      <h2>Request a Quote</h2>
-      <S.Field
-        placeholder="Full Name"
-        id="full_name"
-        required
-        name="full_name"
-        fullWidth
-        variant="outlined"
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <PersonIcon />
-            </InputAdornment>
-          ),
-        }}
-      />
-      <S.Field
-        placeholder="Email"
-        id="email"
-        required
-        name="email"
-        fullWidth
-        variant="outlined"
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <MailIcon />
-            </InputAdornment>
-          ),
-        }}
-      />
-      <S.Field
-        placeholder="Phone"
-        id="phone"
-        required
-        name="full_name"
-        fullWidth
-        variant="outlined"
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <PhoneIcon />
-            </InputAdornment>
-          ),
-        }}
-      />
-      <S.CustomSelect
-        value={products}
-        onChange={handleChange}
-        displayEmpty
-        inputProps={{ "aria-label": "Without label" }}
-        variant="outlined"
-      >
-        <S.CustomMenuItem value="" disabled>
-          Choose a Product
-        </S.CustomMenuItem>
-        <S.CustomMenuItem value="doors">Doors</S.CustomMenuItem>
-        <S.CustomMenuItem value="windows">Windows</S.CustomMenuItem>
-        <S.CustomMenuItem value="railway">Railings</S.CustomMenuItem>
-        <S.CustomMenuItem value="railway">Showers</S.CustomMenuItem>
-      </S.CustomSelect>
-      <S.TextBlock
-        id="outlined-multiline-static"
-        multiline
-        rows={4}
-        placeholder="Tell us about your project..."
-        variant="outlined"
-      />
-      <S.Button name="submit" fullWidth type="submit">
-        Schedule an Appointment
-      </S.Button>
-      {/*</FormControl>*/}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {isLoading && (
+          <S.FormSpinner>
+            <S.CustomSpinner />
+          </S.FormSpinner>
+        )}
+        {!successMessage ? (
+          <>
+            <h2>Request a Quote</h2>
+            <S.Field
+              placeholder="Full Name"
+              id="yourName"
+              required
+              inputRef={register}
+              name="yourName"
+              fullWidth
+              variant="outlined"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <PersonIcon />
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <S.Field
+              placeholder="Email"
+              id="yourEmail"
+              required
+              inputRef={register}
+              name="yourEmail"
+              fullWidth
+              variant="outlined"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <MailIcon />
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <S.Field
+              placeholder="Phone"
+              id="yourPhone"
+              required
+              inputRef={register}
+              name="yourPhone"
+              fullWidth
+              variant="outlined"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <PhoneIcon />
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <FormControl fullWidth variant="outlined">
+              <Controller
+                as={
+                  <S.CustomSelect
+                    value={products}
+                    onChange={handleChange}
+                    displayEmpty
+                    inputProps={{ "aria-label": "Without label" }}
+                    variant="outlined"
+                  >
+                    <S.CustomMenuItem value="" disabled>
+                      Choose a Product
+                    </S.CustomMenuItem>
+                    <S.CustomMenuItem value="doors">Doors</S.CustomMenuItem>
+                    <S.CustomMenuItem value="windows">Windows</S.CustomMenuItem>
+                    <S.CustomMenuItem value="railings">
+                      Railings
+                    </S.CustomMenuItem>
+                    <S.CustomMenuItem value="showers">Showers</S.CustomMenuItem>
+                  </S.CustomSelect>
+                }
+                name="product"
+                control={control}
+              />
+            </FormControl>
+            <S.TextBlock
+              id="yourMessage"
+              multiline
+              rows={4}
+              name="yourMessage"
+              inputRef={register}
+              placeholder="Tell us about your project..."
+              variant="outlined"
+            />
+            <S.Button name="submit" fullWidth type="submit">
+              Schedule an Appointment
+            </S.Button>
+          </>
+        ) : (
+          <S.SuccessMessage>
+            <Typography>{successMessage}</Typography>
+          </S.SuccessMessage>
+        )}
+      </form>
     </S.FormWrapper>
   )
 }
