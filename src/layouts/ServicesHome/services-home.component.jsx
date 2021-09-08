65d45e8de07e46a1ec6ebcec5285cb3a6158fe81
Alex-Shellshock <bryan.r@weareshellshock.com>
2021-09-08 11:00:58 -0500

    mobile card

--- src/layouts/ServicesHome/services-home.component.jsx
+++ src/layouts/ServicesHome/services-home.component.jsx
@@ -4,6 +4,13 @@ import parse from "html-react-parser"
 import Grid from "@material-ui/core/Grid"
 import CustomLink from "../../components/custom-link/custom-link.component"
 import ServiceCard from "../../components/service-card/service-card.component"
+import useMediaQuery from "@material-ui/core/useMediaQuery"
+import theme from "../../theme"
+import { Typography } from "@material-ui/core"
+import Building from "../../assets/icons/building.svg"
+import Home from "../../assets/icons/home.svg"
+import { ArrowForward } from "@material-ui/icons"
+import { graphql, useStaticQuery } from "gatsby"
 
 const ServicesHome = ({
   leftTag,
@@ -18,6 +25,8 @@ const ServicesHome = ({
   rightContent,
   rightLink,
 }) => {
+  const isXS = useMediaQuery(theme.breakpoints.down("xs"))
+
   const centerCard = {
     title: centerTitle,
     content: centerContent,
@@ -31,6 +40,22 @@ const ServicesHome = ({
     link: rightLink,
     isResidentialCard: false,
   }
+
+  const staticQuery = useStaticQuery(graphql`
+    query {
+      blueCard: file(relativePath: { eq: "serviceCardBlue.png" }) {
+        childImageSharp {
+          gatsbyImageData(layout: FULL_WIDTH, quality: 100)
+        }
+      }
+      whiteCard: file(relativePath: { eq: "serviceCardWhite.png" }) {
+        childImageSharp {
+          gatsbyImageData(layout: FULL_WIDTH, quality: 100)
+        }
+      }
+    }
+  `)
+
   return (
     <S.Wrapper>
       <S.CustomContainer>
@@ -46,20 +71,81 @@ const ServicesHome = ({
               )}
             </S.LeftContentWrapper>
           </S.LeftGrid>
-          <Grid item xs={12} md={4}>
+          <S.CardGrid item xs={12} md={4}>
             {centerLink.url && (
               <CustomLink url={centerLink.url}>
-                <ServiceCard {...centerCard} />
+                {isXS ? (
+                  <S.ContentWrapper
+                    img={staticQuery.whiteCard}
+                    style={{ backgroundColor: "#EAF2F6" }}
+                  >
+                    <Home />
+                    {centerTitle && (
+                      <S.CardTitle style={{ color: "#0D5C80" }}>
+                        {centerTitle}
+                      </S.CardTitle>
+                    )}
+                    {centerContent && (
+                      <Typography style={{ color: "#0D5C80" }}>
+                        {centerContent}
+                      </Typography>
+                    )}
+                    {centerLink && (
+                      <S.CardLink>
+                        <CustomLink className="darkLink" url={centerLink.url}>
+                          {" "}
+                          {centerLink.title}
+                        </CustomLink>
+                        <ArrowForward
+                          style={{ color: "#0D5C80" }}
+                          className="arrow"
+                        />
+                      </S.CardLink>
+                    )}
+                  </S.ContentWrapper>
+                ) : (
+                  <ServiceCard {...centerCard} />
+                )}
               </CustomLink>
             )}
-          </Grid>
-          <Grid item xs={12} md={4}>
+          </S.CardGrid>
+          <S.CardGrid item xs={12} md={4}>
             {centerLink.url && (
               <CustomLink url={rightLink.url}>
-                <ServiceCard {...rightCard} />
+                {isXS ? (
+                  <S.ContentWrapper
+                    img={staticQuery.blueCard}
+                    style={{ backgroundColor: "#1593CD" }}
+                  >
+                    <Building />
+                    {rightTitle && (
+                      <S.CardTitle style={{ color: "white" }}>
+                        {rightTitle}
+                      </S.CardTitle>
+                    )}
+                    {rightContent && (
+                      <Typography style={{ color: "white" }}>
+                        {rightContent}
+                      </Typography>
+                    )}
+                    {rightLink && (
+                      <S.CardLink>
+                        <CustomLink className="lightLink" url={rightLink.url}>
+                          {centerLink.title}
+                        </CustomLink>
+                        <ArrowForward
+                          style={{ color: "white" }}
+                          className="arrow"
+                        />
+                      </S.CardLink>
+                    )}
+                  </S.ContentWrapper>
+                ) : (
+                  <ServiceCard {...rightCard} />
+                )}
               </CustomLink>
             )}
-          </Grid>
+          </S.CardGrid>
         </Grid>
       </S.CustomContainer>
     </S.Wrapper>
--- src/layouts/ServicesHome/services-home.styles.jsx
+++ src/layouts/ServicesHome/services-home.styles.jsx
@@ -43,3 +43,76 @@ export const TagText = styled.p`
 export const LeftContentText = styled(Typography)`
   font-size: ${({ theme }) => theme.typography.pxToRem(14)};
 `
+
+export const GridCard = styled(Grid)`
+  min-height: 506px;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: center;
+  border-radius: 4px;
+  ${({ theme }) => theme.breakpoints.down("sm")} {
+    border-radius: unset;
+    padding: 0 !important;
+  }
+  .MuiTypography-root {
+    padding-bottom: 1em;
+    ${({ theme }) => theme.breakpoints.down("sm")} {
+      font-size: ${({ theme }) => theme.typography.pxToRem(14)};
+    }
+  }
+  h3 {
+    ${({ theme }) => theme.breakpoints.down("sm")} {
+      font-size: ${({ theme }) => theme.typography.pxToRem(24)};
+    }
+  }
+  ${({ theme }) => theme.breakpoints.down("sm")} {
+    min-height: unset;
+    h3 {
+      font-size: ${({ theme }) => theme.typography.pxToRem(24)};
+    }
+  }
+`
+
+export const CardLink = styled.div`
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 146%;
+  letter-spacing: 0.02em;
+  .lightLink {
+    color: white;
+  }
+  .darkLink {
+    color: rgb(13, 92, 128);
+  }
+  .arrow {
+    transition: all 0.3s ease;
+    top: 7px;
+    position: relative;
+    margin-left: .3em;
+  }
+  :hover {
+    .arrow {
+      transform: translateX(7px);
+    }
+`
+
+export const ContentWrapper = styled(CustomBgImage)`
+  min-height: 506px;
+  border-radius: 4px;
+  padding: 5em 2em;
+`
+export const CardTitle = styled.h3`
+  font-weight: bold;
+  font-size: 24.4239px;
+  line-height: 120.5%;
+  letter-spacing: 0.01em;
+  padding-top: 1em;
+  padding-bottom: 1em;
+`
+
+export const CardGrid = styled(Grid)`
+  ${({ theme }) => theme.breakpoints.down("sm")} {
+    padding: 0 !important;
+  }
+`
