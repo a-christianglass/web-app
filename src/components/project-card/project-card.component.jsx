5ceb525075733f8611a4a3e874de08973baa77ad
Rick <rick@weareshellshock.com>
2021-09-09 14:12:14 -0500

    use sourceUrl

--- src/components/project-card/project-card.component.jsx
+++ src/components/project-card/project-card.component.jsx
@@ -1,13 +1,12 @@
 import React from "react"
 import * as S from "./project-card.styles.jsx"
-import { getSrc } from "gatsby-plugin-image"
 import Arrow from "../../assets/icons/arrow.svg"
 
 const ProjectCard = ({ image, link }) => {
   return (
     <S.CardContainer>
       <S.Card>
-        <S.CardImage img={getSrc(image?.localFile)} alt={link.title} />
+        <S.CardImage img={image?.sourceUrl} alt={link?.title} />
         <S.LinkWrapper>
           <S.TitleWrapper>
             <span>{link.title}</span>
