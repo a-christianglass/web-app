b0995ca6b33faff70544945fec7d8b209ad26c60
Bryan Valladares <bryanshellshock@users.noreply.github.com>
2021-10-21 11:03:22 -0500

    add new gtag

--- src/html.js
+++ src/html.js
@@ -49,7 +49,15 @@ export default function HTML(props) {
                     `,
           }}
         />
-
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-T9SDGX7');`,
+          }}
+        />
         <script
           dangerouslySetInnerHTML={{
             __html: `
@@ -167,6 +175,14 @@ export default function HTML(props) {
           defer=""
           src="//js.hs-scripts.com/19617179.js"
         />
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T9SDGX7"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          />
+        </noscript>
       </body>
     </html>
   )
